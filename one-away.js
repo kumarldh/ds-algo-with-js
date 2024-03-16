79650c596facceaf44ca20f5b0fb7c8e03549348
kumar chetan sharma <kumarldh@gmail.com>
2024-03-16 20:08:09 +0530

    2 more algos

--- one-away.js
+++ one-away.js
@@ -17,22 +17,24 @@ const oneAway = (str1, str2) => {
   const sortedStr1 = str1.split('').sort();
   const sortedStr2 = str2.split('').sort();
   // possible replacement
-  if (str1.length === str2.length) {
+  if (
+    str1.length === str2.length || // possible replacement
+    str2.length - 1 === str1.length || // possible deletion
+    str2.length + 1 === str1.length // possible addition
+  ) {
     const filteredArr = sortedStr1.filter((e) => !sortedStr2.includes(e));
     return filteredArr.length === 1;
   }
-  /**
-   *  || // possible replacement
-    str2.length - 1 === str1.length || // possible deletion
-    str2.length + 1 === str1.length // possible addition
-   */
   return false;
 };
 [
-  ['pale', 'ple'],
+  ['pale', 'plea'],
+  ['pale', 'ale'],
+  ['pale', 'pke'],
   ['pales', 'pale'],
   ['pale', 'bale'],
   ['pale', 'bake'],
+  ['cake', 'bake'],
 ].forEach((e) =>
   console.log(`oneWay('${e[0]}', '${e[1]}') -> ${oneAway(e[0], e[1])}`)
 );
---
+++ string-compression.js
@@ -0,0 +1,22 @@
+/**
+ * Implement a method to perform basic string compression using the counts
+ * of repeated characters. For example, the string aabcccccaaa would become a2blc5a3. If the
+ * "compressed" string would not become smaller than the original string, your method should return
+ * the original string. You can assume the string has only uppercase and lowercase letters (a - z).
+ *
+ * compressString('aabcccccaaa') => 'a2blc5a3'
+ */
+
+const compressString = (str) => {
+  let out = '';
+  const splitStr = str.split('').forEach((e, idx) => {
+    if (idx > 0) {
+      out = out + str[idx];
+    }
+  });
+  return out;
+};
+
+['aabcccccaaa', 'aashna'].forEach((e) =>
+  console.log(`compressString('${e}') => '${compressString(e)}'`)
+);
