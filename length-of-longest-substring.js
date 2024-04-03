d7049ea5c6eb548b1c0aaf5bae2be1e78e494c03
Kumar Chetan Sharma <kumarldh@gmail.com>
2024-04-03 12:32:54 +0530

    Create length-of-longest-substring.js

---
+++ length-of-longest-substring.js
@@ -0,0 +1,27 @@
+const lengthOfLongestSubstring = (str) => {
+  const strLength = str.length;
+  let startIndex = 0;
+  let stopIndex = 0;
+  let m = new Map();
+  for (let i = 0; i < strLength; i++) {
+    console.log(`i = ${i}, str[i] = ${str[i]}`);
+    if (!m.has(str[i])) {
+      console.log(
+        `not found in Map m, setting ${str[i]} at ${i} in Map `,
+        ...m.values(),
+      );
+      m.set(str[i], i);
+    } else {
+      const indexAtWhichThisCharExistsInSet = m.get(str[i]);
+      console.log(
+        `Found in Map m at ${indexAtWhichThisCharExistsInSet} at ${i} in Map `,
+        ...m.values(),
+      );
+    }
+  }
+  //console.log([startIndex, stopIndex, m.size, ...m.keys(), ...m.values()]);
+};
+//const tcs = ["abcabcbb", "bbbb", "pwwkew", "ABDEFGABEF", "GEEKSFORGEEKS"];
+const tcs = ["abcabcbb"];
+tcs.forEach((s) => lengthOfLongestSubstring(s));
+//[abc] [b] [wke, kew] [abdefg, BDEFGA, DEFGAB,], [eksforg, ksforge]
